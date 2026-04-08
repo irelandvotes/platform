c281ed790f2d24c7ffb6b115767c72124957df88
Glen <contact@irelandvotes.com>
2026-04-08 22:07:12 +0100

    Fixes and added papaparse types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config.js";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript";
+import nextTs from "eslint-config-next/typescript.js";
 import typescript from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
