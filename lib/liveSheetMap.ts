5a0e9a4453701145b6f2bf4eb16bd6d1f7712d17
Glen <contact@irelandvotes.com>
2026-05-23 17:01:42 +0100

    disabled live tally

--- lib/liveSheetMap.ts
+++ lib/liveSheetMap.ts
@@ -9,7 +9,7 @@ Record<string, LiveSheetConfig> = {
   "ireland-dail-2026-dublin-central": {
 
      tally:
-      "https://docs.google.com/spreadsheets/d/10M1Aq9D7oxU_asJ9ZGyf7P1qIQEKN6k86tgA0LJmtK8/export?format=csv",
+      "",
 
      official:
       "https://docs.google.com/spreadsheets/d/1xuBzJQ0Fje8iEDc50gvb6iSCg1-1bZen87yFwUXrhXw/export?format=csv"
@@ -18,7 +18,7 @@ Record<string, LiveSheetConfig> = {
   "ireland-dail-2026-galway-west": {
 
      tally:
-      "https://docs.google.com/spreadsheets/d/1wIPwSuSbD-L-2Iei7rYbetCVo3T0BsoaMStc7PUlLGU/export?format=csv",
+      "",
 
      official:
       "https://docs.google.com/spreadsheets/d/1CpAo88EitVbmKT47bFlmonhCZ4sb_wOYM4SKx-pWegE/export?format=csv"
