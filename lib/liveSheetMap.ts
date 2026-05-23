c7f218ad909161d8d24ccfeecb4d3129dadb2854
Glen <contact@irelandvotes.com>
2026-05-24 00:44:29 +0100

    dub c data

--- lib/liveSheetMap.ts
+++ lib/liveSheetMap.ts
@@ -6,15 +6,6 @@ type LiveSheetConfig = {
 export const liveSheetMap:
 Record<string, LiveSheetConfig> = {
 
-  "ireland-dail-2026-dublin-central": {
-
-     tally:
-      "https://docs.google.com/spreadsheets/d/10M1Aq9D7oxU_asJ9ZGyf7P1qIQEKN6k86tgA0LJmtK8/export?format=csv",
-
-     official:
-      "https://docs.google.com/spreadsheets/d/1xuBzJQ0Fje8iEDc50gvb6iSCg1-1bZen87yFwUXrhXw/export?format=csv"
-  },
-
   "ireland-dail-2026-galway-west": {
 
      tally:
