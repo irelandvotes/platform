dab9f5b921801d154c6b8c6391585ce6e0f67978
Glen <contact@irelandvotes.com>
2026-05-24 21:39:38 +0100

    de-live

--- lib/liveSheetMap.ts
+++ lib/liveSheetMap.ts
@@ -6,13 +6,5 @@ type LiveSheetConfig = {
 export const liveSheetMap:
 Record<string, LiveSheetConfig> = {
 
-  "ireland-dail-2026-galway-west": {
-
-     tally:
-      "https://docs.google.com/spreadsheets/d/1wIPwSuSbD-L-2Iei7rYbetCVo3T0BsoaMStc7PUlLGU/export?format=csv",
-
-     official:
-      "https://docs.google.com/spreadsheets/d/1CpAo88EitVbmKT47bFlmonhCZ4sb_wOYM4SKx-pWegE/export?format=csv"
-  }
 
 };
